d869c82e0ca719edb9d2a16f8600ce60355623c7
mrsol <mrrsol.it@gmail.com>
2026-08-20 15:21:28 +0700

    switch case and if else

--- src/Main.java
+++ src/Main.java
@@ -8,45 +8,44 @@ public class Main {
             System.out.println( "i = " + i);
             System.out.println("Neymar jr is the best player in the world");
         }
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your balance: ");
+        int balance = input.nextInt();
+        int option = 4;
+        do {
+            System.out.println("""
+                1. Check Balance
+                2. Deposit
+                3. Withdraw
+                4. Exit
+                """);
+            System.out.print("Enter your option: ");
+            option = input.nextInt();
+            if (option >=1 && option <= 3) {
+                switch (option) {
+                    case 1 -> System.out.println("Your balance is: " + balance);
+                    case 2 -> {
+                        System.out.print("Enter the amount to deposit: ");
+                        int deposit = input.nextInt();
+                        balance += deposit;
+                        System.out.print("Your new balance is: " + balance);
+                    }
+                    case 3 -> {
+                        System.out.print("Enter the amount to withdraw: ");
+                        int withdraw = input.nextInt();
+                        balance -= withdraw;
+                    }
+                    default -> {
+                        System.out.println("Invalid option");
+                    }
+                }
+            }
+
+        } while (true);
+
     }
-//
-//        Scanner input = new Scanner(System.in);
-//
-//        System.out.print("Enter your balance: ");
-//        int balance = input.nextInt();
-//        int option = 4;
-//        do {
-//            System.out.println("""
-//                1. Check Balance
-//                2. Deposit
-//                3. Withdraw
-//                4. Exit
-//                """);
-//            System.out.print("Enter your option: ");
-//            option = input.nextInt();
-//            if (option >=1 && option <= 3) {
-//                switch (option) {
-//                    case 1 -> System.out.println("Your balance is: " + balance);
-//                    case 2 -> {
-//                        System.out.print("Enter the amount to deposit: ");
-//                        int deposit = input.nextInt();
-//                        balance += deposit;
-//                        System.out.print("Your new balance is: " + balance);
-//                    }
-//                    case 3 -> {
-//                        System.out.print("Enter the amount to withdraw: ");
-//                        int withdraw = input.nextInt();
-//                        balance -= withdraw;
-//                    }
-//                    default -> {
-//                        System.out.println("Invalid option");
-//                    }
-//                }
-//            }
-//
-//        } while (true);
-//
-//    }
 
 
 }
