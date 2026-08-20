3256fca9b07764ce93b0a880189272a6b6e957d4
mrsol <mrrsol.it@gmail.com>
2026-08-20 15:09:58 +0700

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Calculator {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("CALCULATOR (num operator num) : ");
+//        String inputString = input.nextLine();
+//        Scanner scanner = new Scanner(inputString);
+//        double num1 = scanner.nextDouble();
+//        String op = scanner.next();
+//        double num2 = scanner.nextDouble();
+//        scanner.close();
+//        double result = switch (op) {
+//            case "+" -> num1 + num2;
+//            case "-" -> num1 - num2;
+//            case "*" -> num1 * num2;
+//            case "/" -> num1 / num2;
+//            default -> 0;
+//        };
+//        System.out.println("Result: "+result);
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int number = input.nextInt();
+//        int sum = 0;
+//
+//        for (int i = 1; i <=number  ; i++) {
+//                sum+=i;
+//            System.out.print(i + " + " );
+//        }
+//        System.out.printf("\b\b = " + sum);
+//    }
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+            System.out.print(i + " * ");
+        }
+        System.out.println( "\b\b = " + factorial);
+    }
+}
