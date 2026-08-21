789ab02a61828996c29936b5bfe44e03bcc1e704
mrsol <mrrsol.it@gmail.com>
2026-08-21 15:32:22 +0700

    inherit

--- src/InteritDemo.java
+++ src/InteritDemo.java
@@ -1,2 +1,26 @@
 public class InteritDemo {
+
+    static void main() {
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.scream();
+    }
+
+}
+
+class Animal {
+
+    void eat () {
+        System.out.println("Animal is eating");
+    }
+
+}
+
+class Dog extends Animal {
+
+    void scream () {
+        System.out.println("Dog is screaming");
+    }
+
 }
